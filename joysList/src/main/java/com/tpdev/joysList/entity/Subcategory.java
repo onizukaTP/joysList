d1eb3a7f81b04c92fbd7e7cfc8c6c471bfb91c6f
onizukaTP <tharunprabu2418@gmail.com>
2026-05-21 15:57:47 +0530

    refactor: change generation type to identity

--- joysList/src/main/java/com/tpdev/joysList/entity/Subcategory.java
+++ joysList/src/main/java/com/tpdev/joysList/entity/Subcategory.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Builder
 public class Subcategory {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
