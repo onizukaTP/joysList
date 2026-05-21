75f5292fd5f19286c5d85873d4d74923f25efc8a
onizukaTP <tharunprabu2418@gmail.com>
2026-05-21 16:01:17 +0530

    refactor: add getter to get user entity

--- joysList/src/main/java/com/tpdev/joysList/entity/CustomUserDetails.java
+++ joysList/src/main/java/com/tpdev/joysList/entity/CustomUserDetails.java
@@ -49,4 +49,8 @@ public class CustomUserDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public UserEntity getUser() {
+        return user;
+    }
 }
