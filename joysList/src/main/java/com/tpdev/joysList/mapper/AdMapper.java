9d005d0b30d7cac6d444347e31124a8f1c647f68
onizukaTP <tharunprabu2418@gmail.com>
2026-05-21 16:03:28 +0530

    feat: add method to convert dto to specific community and resume ad

--- joysList/src/main/java/com/tpdev/joysList/mapper/AdMapper.java
+++ joysList/src/main/java/com/tpdev/joysList/mapper/AdMapper.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class AdMapper {
 
-    public HousingAd toHousingAd(AdRequestDto dto, Subcategory subcategory) {
+    public HousingAd toHousingAd(AdRequestDto dto, Subcategory subcategory, UserEntity postedBy) {
         HousingAd ad = new HousingAd();
-        setCommonFields(ad, dto, subcategory);
+        setCommonFields(ad, dto, subcategory, postedBy);
         ad.setNumberOfBeds(dto.getNumberOfBeds());
         ad.setNumberOfBathrooms(dto.getNumberOfBathrooms());
         ad.setCatsOk(dto.getCatsOk());
@@ -23,32 +23,32 @@ public class AdMapper {
         return ad;
     }
 
-    public ForSaleAd toForSaleAd(AdRequestDto dto, Subcategory subcategory) {
+    public ForSaleAd toForSaleAd(AdRequestDto dto, Subcategory subcategory, UserEntity postedBy) {
         ForSaleAd ad = new ForSaleAd();
-        setCommonFields(ad, dto, subcategory);
+        setCommonFields(ad, dto, subcategory, postedBy);
         ad.setSoldBy(dto.getSoldBy());
         ad.setCondition(dto.getCondition());
         return ad;
     }
 
-    public EventAd toEventAd(AdRequestDto dto, Subcategory subcategory) {
+    public EventAd toEventAd(AdRequestDto dto, Subcategory subcategory, UserEntity postedBy) {
         EventAd ad = new EventAd();
-        setCommonFields(ad, dto, subcategory);
+        setCommonFields(ad, dto, subcategory, postedBy);
         ad.setEventTypes(dto.getEventTypes());
         return ad;
     }
 
-    public GigsAd toGigsAd(AdRequestDto dto, Subcategory subcategory) {
+    public GigsAd toGigsAd(AdRequestDto dto, Subcategory subcategory, UserEntity postedBy) {
         GigsAd ad = new GigsAd();
-        setCommonFields(ad, dto, subcategory);
+        setCommonFields(ad, dto, subcategory, postedBy);
         ad.setGigs(dto.getGigs());
         ad.setStatus(dto.getStatus());
         return ad;
     }
 
-    public JobAd toJobAd(AdRequestDto dto, Subcategory subcategory) {
+    public JobAd toJobAd(AdRequestDto dto, Subcategory subcategory, UserEntity postedBy) {
         JobAd ad = new JobAd();
-        setCommonFields(ad, dto, subcategory);
+        setCommonFields(ad, dto, subcategory, postedBy);
         ad.setNonProfitOrganization(dto.getNonProfitOrganization());
         ad.setInternship(dto.getInternship());
         ad.setTelecommutingOk(dto.getTelecommutingOk());
@@ -57,14 +57,35 @@ public class AdMapper {
         return ad;
     }
 
-    public ServiceAd toServiceAd(AdRequestDto dto, Subcategory subcategory) {
+    public ServiceAd toServiceAd(AdRequestDto dto, Subcategory subcategory, UserEntity postedBy) {
         ServiceAd ad = new ServiceAd();
-        setCommonFields(ad, dto, subcategory);
+        setCommonFields(ad, dto, subcategory, postedBy);
         ad.setServiceType(dto.getServiceType());
         return ad;
     }
 
-    public void setCommonFields(Ad ad, AdRequestDto dto, Subcategory subcategory) {
+    public CommunityAd toCommunityAd(AdRequestDto dto, Subcategory subcategory, UserEntity postedBy) {
+        CommunityAd ad = new CommunityAd();
+        setCommonFields(ad, dto, subcategory, postedBy);
+        ad.setCommunityType(dto.getCommunityType());
+        ad.setLostOrFound(dto.getLostOrFound());
+        return ad;
+    }
+
+    public ResumeAd toResumeAd(AdRequestDto dto, Subcategory subcategory, UserEntity postedBy) {
+        ResumeAd ad = new ResumeAd();
+        setCommonFields(ad, dto, subcategory, postedBy);
+        ad.setAvailableMornings(dto.getAvailableMornings());
+        ad.setAvailableAfternoons(dto.getAvailableAfternoons());
+        ad.setAvailableEvenings(dto.getAvailableEvenings());
+        ad.setAvailableOvernights(dto.getAvailableOvernights());
+        ad.setAvailableWeekdays(dto.getAvailableWeekdays());
+        ad.setAvailableWeekends(dto.getAvailableWeekends());
+        ad.setEducationCompleted(dto.getEducationCompleted());
+        return ad;
+    }
+
+    public void setCommonFields(Ad ad, AdRequestDto dto, Subcategory subcategory, UserEntity postedBy) {
         ad.setTitle(dto.getTitle());
         ad.setDescription(dto.getDescription());
         ad.setPrice(dto.getPrice());
@@ -75,5 +96,6 @@ public class AdMapper {
         ad.setDeliveryAvailable(dto.getDeliveryAvailable());
         ad.setSubcategory(subcategory);
         ad.setCreatedAt(dto.getCreatedAt());
+        ad.setPostedBy(postedBy);
     }
 }
