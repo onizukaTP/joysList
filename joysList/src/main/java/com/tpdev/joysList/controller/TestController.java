d6b8ca4a7b1222eb63f6f066cbe1fd4e6415e149
onizukaTP <tharunprabu2418@gmail.com>
2026-05-21 14:24:17 +0530

    docs: add logs

--- joysList/src/main/java/com/tpdev/joysList/controller/TestController.java
+++ joysList/src/main/java/com/tpdev/joysList/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.tpdev.joysList.controller;
 
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +9,11 @@ import java.util.Date;
 
 @RestController
 @RequestMapping("/hello")
+@Slf4j
 public class TestController {
     @GetMapping
     public String hello() {
+        log.info("Testing...");
         return "Hello, User.\n "
                 + "Time is now " + new Date() + ".";
     }
