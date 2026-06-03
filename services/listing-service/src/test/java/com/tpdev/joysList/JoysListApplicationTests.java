c3227b0aac59f0b39de24573b30e183084543122
onizukaTP <tharunprabu2418@gmail.com>
2026-06-03 16:41:24 +0530

    chore: ignore kafka for testing

--- services/listing-service/src/test/java/com/tpdev/joysList/JoysListApplicationTests.java
+++ services/listing-service/src/test/java/com/tpdev/joysList/JoysListApplicationTests.java
@@ -3,7 +3,12 @@ package com.tpdev.joysList;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+		properties = {
+				"spring.kafka.admin.auto-create=false",
+				"spring.kafka.admin.fail-fast=false"
+		}
+)
 class JoysListApplicationTests {
 
 	@Test
