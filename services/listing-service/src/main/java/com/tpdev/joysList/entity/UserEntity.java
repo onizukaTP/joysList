419ed15d39bb1d83f4d410b883f11e32910b9f11
onizukaTP <tharunprabu2418@gmail.com>
2026-06-03 14:32:37 +0530

    refactor: change unique setting for field of username and email to true

--- services/listing-service/src/main/java/com/tpdev/joysList/entity/UserEntity.java
+++ services/listing-service/src/main/java/com/tpdev/joysList/entity/UserEntity.java
@@ -15,7 +15,11 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false, unique = true)
     private String email;
     private String password;
 
