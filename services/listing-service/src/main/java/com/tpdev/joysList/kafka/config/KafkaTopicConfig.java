c604d4921b03e731d10931f22c627f7b066da85b
onizukaTP <tharunprabu2418@gmail.com>
2026-06-08 10:42:08 +0530

    feat: add user-events topic to kafka

--- services/listing-service/src/main/java/com/tpdev/joysList/kafka/config/KafkaTopicConfig.java
+++ services/listing-service/src/main/java/com/tpdev/joysList/kafka/config/KafkaTopicConfig.java
@@ -16,4 +16,13 @@ public class KafkaTopicConfig {
                 .replicas(1)
                 .build();
     }
+
+    @Bean
+    public NewTopic userEventsTopic() {
+        return TopicBuilder
+                .name("user-events")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
 }
