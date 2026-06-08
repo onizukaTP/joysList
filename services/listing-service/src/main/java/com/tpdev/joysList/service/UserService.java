1069a31b68f3df215ebb3be3ae005c1bdf9a7675
onizukaTP <tharunprabu2418@gmail.com>
2026-06-08 10:40:58 +0530

    feat: add kafka publisher to publish user registered event

--- services/listing-service/src/main/java/com/tpdev/joysList/service/UserService.java
+++ services/listing-service/src/main/java/com/tpdev/joysList/service/UserService.java
@@ -1,9 +1,12 @@
 package com.tpdev.joysList.service;
 
+import com.tpdev.events.UserRegisteredEvent;
 import com.tpdev.joysList.entity.UserEntity;
 import com.tpdev.joysList.entity.enums.Role;
+import com.tpdev.joysList.kafka.producer.UserEventProducer;
 import com.tpdev.joysList.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +14,27 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
+
+    private final UserEventProducer userEventProducer;
 
     public UserEntity createUser(UserEntity user) {
         // encode password
         user.setPassword(encoder.encode(user.getPassword()));
 
         userRepository.save(user);
+
+        userEventProducer.publishUserRegistered(
+                new UserRegisteredEvent(
+                        user.getId(),
+                        user.getEmail(),
+                        user.getUsername()
+                )
+        );
+        log.info("Published User Registered Event");
         return user;
     }
 
