bd0b3b56f9d9ba8ea63dc24918f03c8fcebcde36
onizukaTP <tharunprabu2418@gmail.com>
2026-06-09 16:57:15 +0530

    feat(notification-service): add method to send email for user registration and testing

--- services/notification-service/src/main/java/com/notificationservice/service/EmailService.java
+++ services/notification-service/src/main/java/com/notificationservice/service/EmailService.java
@@ -3,12 +3,21 @@ package com.notificationservice.service;
 import com.tpdev.events.AdCreatedEvent;
 import com.tpdev.events.AdDeletedEvent;
 import com.tpdev.events.UserRegisteredEvent;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class EmailService {
+
+    private final JavaMailSender mailSender;
 
     public void sendAdCreatedEmail(AdCreatedEvent event) {
         log.info("Email sent for creation event for ad {}", event.getTitle());
@@ -18,7 +27,50 @@ public class EmailService {
         log.info("Email send for deletion event for ad {}", event.getTitle());
     }
 
-    public void sendUserRegisteredEmail(UserRegisteredEvent event) {
-        log.info("Sending welcome email to {}", event.getEmail());
+    // just for logs
+    public void printWelcomeMessage(UserRegisteredEvent event) {
+        log.info("Welcome, {}", event.getEmail());
+    }
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendWelcomeEmail(UserRegisteredEvent event) {
+
+        SimpleMailMessage mail = new SimpleMailMessage();
+
+        mail.setFrom(from);
+
+        mail.setTo(event.getEmail());
+
+        mail.setSubject("Welcome to JoysList");
+
+        mail.setText(
+                "Hello "
+                        + event.getUsername()
+                        + ", your account has been created successfully."
+        );
+        try {
+            mailSender.send(mail);
+            log.info("Welcome email sent to {}", event.getEmail());
+        } catch (Exception e) {
+            log.error("Email failed", e);
+        }
+    }
+
+    public void sendTestMail() {
+        SimpleMailMessage mail = new SimpleMailMessage();
+
+        mail.setFrom(from);
+        mail.setTo(from);
+        mail.setSubject("Test");
+        mail.setText("Hello");
+
+        try {
+            mailSender.send(mail);
+            log.info("Mail Sent Successfully");
+        } catch (MailException e) {
+            log.error("Error Sending Mail: {}", e.getMessage(), e);
+        }
     }
 }
